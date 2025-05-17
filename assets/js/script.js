3cef1de34c98eee342842d9ff9e108bc3557b6af
Eduard <47057625+Eduardick1@users.noreply.github.com>
2025-05-17 19:43:37 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+const noOverflowClass = "no-overflow";
+
+function openModal(modalID) {
+  document.getElementById(modalID).showModal();
+}
+function closeModal(modalID) {
+  document.getElementById(modalID).close();
+}
+
+document.addEventListener("click", (e) => {
+  const { target } = e;
+  const widget = target.closest(".widget");
+  if (widget) {
+    switch (widget.dataset.controls) {
+      case "modal":
+        return openModal(widget.getAttribute("aria-controls"));
+    }
+  }
+  const modal = target.closest(".modal");
+  if (modal && target.closest(".modal-close")) return closeModal(modal.id);
+});
+
+const callbackWidget = document.querySelector(".widget.callback");
+function highlightWidget() {
+  const treshhold = callbackWidget.getBoundingClientRect().height;
+  const totalH = document.body.scrollHeight;
+  const windowH = window.innerHeight;
+  const scrollY = window.scrollY;
+  const isBottom = scrollY + windowH + treshhold >= totalH;
+  callbackWidget.classList.toggle("contrast", !isBottom);
+}
+highlightWidget();
+let debounce;
+document.addEventListener("scroll", () => {
+  clearTimeout(debounce);
+  debounce = setTimeout(highlightWidget, 500);
+});
+
+const invalidClass = "invalid";
+const phoneInput = document.getElementById("phoneInput");
+// phoneInput.addEventListener("input", function () {
+//   this.classList.remove(invalidClass);
+// });
+
+const mask = IMask(phoneInput, {
+  mask: "+{20} (000) 000-00-00",
+});
+
+const form = document.getElementById("form-callback");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(mask);
+  if (!mask.unmaskedValue || mask.unmaskedValue.length < 11)
+    return phoneInput.classList.add(invalidClass);
+  form.closest(".modal").classList.add("final");
+  phoneInput.value = "";
+});
